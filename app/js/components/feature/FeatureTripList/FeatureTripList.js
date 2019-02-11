70a0b9ff820734a499e21858324dafad3d4caf63
chaiyaphon <chaiyaphon.c@scale360solutions.com>
2019-02-11 20:03:37 +0700

    init feature to project

---
+++ app/js/components/feature/FeatureTripList/FeatureTripList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FeatureTripList extends Component {
+  render() {
+    return (
+      <div className="feature-trip-list-container">
+        test
+      </div>
+    )
+  }
+}
