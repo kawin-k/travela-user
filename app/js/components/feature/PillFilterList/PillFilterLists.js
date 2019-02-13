360369a19a067e1f54eef8bd0be64e929755ff26
chaiyaphon <chaiyaphon.c@scale360solutions.com>
2019-02-13 19:16:57 +0700

    add PillFilter Component

--- app/js/components/feature/PillFilterList/PillFilterLists.js
+++ app/js/components/feature/PillFilterList/PillFilterLists.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import './index.scss'
 
 class PillFilterList extends Component {
+  static propTypes = {
+    optionList: PropTypes.array.isRequired,
+    handlerOnSelectPill: PropTypes.func.isRequired,
+  }
+
+  state = {
+    selectedIndex: -1
+  }
+
+  onSelectPill = (index) => {
+    const { selectedIndex: curSelectedIndex } = this.state
+    const selectedIndex = curSelectedIndex === index ? -1 : index
+
+    this.setState({
+      selectedIndex,
+    })
+
+    const { handlerOnSelectPill, optionList } = this.props 
+
+    if (handlerOnSelectPill) {
+      const valueForCallBack = selectedIndex === -1
+        ? ''
+        : optionList[selectedIndex]
+
+      handlerOnSelectPill(valueForCallBack)
+    }
+  }
+
+  renderPillList = () => {
+    const { selectedIndex } = this.state
+    const { optionList } = this.props
+
+    return (
+      optionList.map((option, index) => {
+        const isSelected = selectedIndex == index
+        
+        return (
+          <div
+            className={classNames(
+              'pill-filter-option-wrapper',
+              { active: isSelected }
+            )}
+            onClick={() => this.onSelectPill(index)}
+            key={index}
+          >
+            {option}
+          </div>
+        )
+      })
+    )
+  }
+
   render() {
     return (
       <div className="pill-filter-list-container">
-        PillFilterList
+        {this.renderPillList()}
       </div>
     )
   }
--- app/js/pages/Home/Home.js
+++ app/js/pages/Home/Home.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
-import { MainLayout } from '../../components/layout'
+
+import MainLayout from '../../components/layout/MainLayout'
+import { Tab } from '../../components/common'
+import { PillFilterList } from '../../components/feature'
 import './index.scss'
 
 class Home extends Component {
+  handlerOnSelectPillFilterList = (filterName) => {
+    console.log('filterName', filterName)
+  }
+
   render() {
     return (
-      <MainLayout>
-        <div className="home-page-container">
-          Home
-        </div>
-      </MainLayout>
+      <div className="home-page-container">
+        <MainLayout>
+          <h3>Travela</h3>
+          <PillFilterList
+            handlerOnSelectPill={this.handlerOnSelectPillFilterList}
+            optionList={['Bangkok', 'Chiang Mai']}
+            // optionList={['Bangkok', 'Chiang Mai', 'Chiang Rai', 'Nan']}
+          />
+          <Tab>
+            <Tab.TabContent label="Missions">
+              <div>mission container</div>
+            </Tab.TabContent>
+            <Tab.TabContent label="Details">
+              <div>detail container</div>
+            </Tab.TabContent>
+          </Tab>
+        </MainLayout>
+      </div>
     )
   }
 }
